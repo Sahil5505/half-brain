d466802ee6b4414af37ce6d7f86cb81eee62fa8e
sahil5505 <sahilrevankar20co21@gmail.com>
2024-07-31 09:26:34 +0530

    scoping data to identifier

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,5 @@
 import { mutation, query } from './_generated/server';
-import { v } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 
 
 
@@ -7,12 +7,20 @@ import { v } from 'convex/values';
 
 export const getDocuments = query({
     async handler(ctx) {
-        return await ctx.db.query('documents').collect();
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        console.log(userId);
+
+        if(!userId) {
+
+                return[];
+        }
+
+
+        return await ctx.db.query('documents')
+        .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier',userId)).collect();  
     },
 })
-
-
-
 
 
 
@@ -21,8 +29,20 @@ export const createDocument = mutation({
         title: v.string()
     },
     async handler(ctx, args) {
+
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        console.log(userId);
+
+        if(!userId) {
+            throw new ConvexError('Unauthorized');  
+        }
+
+
+
         await ctx.db.insert('documents', {
-            title: args.title
+            title: args.title,
+            tokenIdentifier: userId
         });
     }
 }); 
